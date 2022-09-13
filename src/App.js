c843acd6039765b4b78c8b924bcb7e180f72c343
GouravSingh2580 <49956754+GouravSingh2580@users.noreply.github.com>
2022-09-13 22:32:43 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import React, { Component } from 'react';
-import './style.css';
-// import logo from ''
-import Counter from './Counter'
+import React, { useEffect, useState } from 'react';
 
-export default class App extends Component {
-  constructor(){
-    super()
-    this.state={
-      count:0
-    }
-  }
-  
-  componentDidUpdate() {
-    console.log('updated');
-  }
-  componentDidMount() {
-    console.log('mounted');
-  }
-  do(){
-    this.setState({count:this.state.count+1})
-  }
-  render() {
-    return (
-      <>
-        <h1>Hello + ${this.state.count}!</h1>
-        <p>Start editing to see some magic happen :)</p>
-        <img
-          style={{ height: 100, width: 150 }}
-          src="https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697"
-        />
-        <button onClick={()=>{this.do()}}> Click Me </button>
-        <Counter/>
-      </>
-    );
-  }
+export default function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+        const json = await response.json();
+        setUsers(json);
+        console.log(json);
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return <>{users && users.map((u) => <h1>{u.name}</h1>)}</>;
 }
